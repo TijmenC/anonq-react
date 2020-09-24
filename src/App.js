faaabbdd29a60787a61871f435d27763626c2e54
TimonC <58507497+TimonC@users.noreply.github.com>
2020-09-24 14:27:28 +0200

    Added new Navigationbar + Styling

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-<div>hoi
+<div className="App">hoi
 </div>
   );
 }
--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar'
 import logo from '../images/logo.png';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styling/Navigationbar.css';
+import { Button,Navbar,Nav,NavDropdown,Form,FormControl } from 'react-bootstrap';
 
 
 function Navigationbar() {
     return (
-    <Navbar bg="white" variant="white">
-    <Navbar.Brand href="#home">
-      <img
-        alt=""
-        src={logo}
-        width="150"
-        height="75"
-        className="d-inline-block align-top"
-      />{' '}
-      React Bootstrap
-    </Navbar.Brand>
-  </Navbar>
+      <Navbar bg="dark" variant="dark">
+      <Nav className="container-fluid">
+      <Navbar.Brand href="#home">
+        <img src={logo} width="100" height="50" className="d-inline-block align-top" />
+        AnonQ
+      </Navbar.Brand>
+      <Nav.Item className="ml-auto">
+        <Button variant="outline-primary">Search</Button>
+        </Nav.Item>
+      </Nav>
+    </Navbar>
     );
 }
 export default Navigationbar;
