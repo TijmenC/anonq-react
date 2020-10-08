3da7cae40125f6f339cb6bddac6c7207477b934e
TimonC <58507497+TimonC@users.noreply.github.com>
2020-10-08 14:26:32 +0200

    id fixent

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,22 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from "axios";
 import Question from './Question'
 
-
-
-const QuestionUri = "https://localhost:44348/api/Question/"
+const id = 1;
 
 function Home() {
     const [questionList, setQuestionList] = useState([]);
     const [id, setID] = useState(1);
 
-
-    useEffect(() => {
+    function handleClick() {
         const min = 1;
         const max = 2;
         const rand = Math.round(min + Math.random() * (max - min));
-       
+      }
 
-        axios.get("https://localhost:44348/api/Question/" + rand).then((res) => {
+     
+    useEffect(() => {
+        axios.get("https://localhost:44348/api/Question/" + id).then((res) => {
             const newQuestionList = res.data;
             setQuestionList(newQuestionList);
             console.log(questionList);
