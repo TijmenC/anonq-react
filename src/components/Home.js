9f29bfd7151d0f3b70b1c14f6a97ebc1267f8888
TimonC <58507497+TimonC@users.noreply.github.com>
2020-10-09 11:46:32 +0200

    Added proper styling for the homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,9 @@ import Question from './Question'
 import { Row, Col, Image, Figure, Button } from 'react-bootstrap';
 import skip from '../images/skip.png'
 import report from '../images/report.png';
-import {
-    Link,
-} from "react-router-dom";
+import { Link, } from "react-router-dom";
+
+
 
 
 
@@ -31,36 +31,38 @@ function Home() {
     return (
         <div>
             <Question question={questionList} />
-            <Row>
-                <Col>
-                    <div className="float-left">
-                        <figure>
-                            <Link to="/" >
-                                <Figure.Image
-                                    width={50}
-                                    height={50}
-                                    src={report}
-                                />
-                            </Link>
-                            <figcaption class="figure-caption text-middle">Report</figcaption>
-                        </figure>
-                    </div >
-                </Col>
-                <Col>
-                    <div className="float-right">
-                        <figure>
-                            <Link to="/" onClick={handleChange}>
-                                <Figure.Image
-                                    width={50}
-                                    height={50}
-                                    src={skip}
-                                />
-                            </Link>
-                            <figcaption class="figure-caption text-middle">Skip</figcaption>
-                        </figure>
-                    </div>
-                </Col>
-            </Row>
+            <div className="rounded container">
+                <Row>
+                    <Col>
+                        <div className="float-left">
+                            <figure>
+                                <Link to="/" >
+                                    <Figure.Image
+                                        width={50}
+                                        height={50}
+                                        src={report}
+                                    />
+                                </Link>
+                                <figcaption class="figure-caption text-middle">Report</figcaption>
+                            </figure>
+                        </div >
+                    </Col>
+                    <Col>
+                        <div className="float-right">
+                            <figure>
+                                <Link to="/" onClick={handleChange}>
+                                    <Figure.Image
+                                        width={50}
+                                        height={50}
+                                        src={skip}
+                                    />
+                                </Link>
+                                <figcaption class="figure-caption text-middle">Skip</figcaption>
+                            </figure>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
         </div>
     );
 }
