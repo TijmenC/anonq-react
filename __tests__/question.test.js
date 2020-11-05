f67bae15161ede9462d6e894005141942c0f359c
TimonC <58507497+TimonC@users.noreply.github.com>
2020-11-05 14:56:32 +0100

    Added a test + presets

---
+++ __tests__/question.test.js
@@ -0,0 +1,26 @@
+import axios from "axios"
+import HomePage from "../src/pages/HomePage"
+
+jest.mock('axios')
+
+describe("Subject routing tests", () => {
+
+    it("Should get all questions", async () => {
+        const questions = [
+        {
+            question: { title: 'Title', description: 'Description', tag: 'Tag', commentsenabled: 'true' },
+            poll: [{ poll: 'poll' }]
+        },
+        {
+            question: { title: 'Title2', description: 'Description2', tag: 'Tag2', commentsenabled: 'true' },
+            poll: [{ poll: 'poll2' }]
+        }
+
+        ]
+        const resp = {data: questions}
+        axios.get.mockResolvedValue(resp)
+        return HomePage.useEffect().then(data => expect(data).ToEqual(questions))
+      
+       
+    });
+})
